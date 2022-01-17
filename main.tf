a96f30b85d11639d2b0172004602bf33cf6bacff
RustamGimadiev <gimadiev.kzn@yandex.ru>
2022-01-17 15:52:57 +0300

    use aws meta module

--- main.tf
+++ main.tf
@@ -2,20 +2,44 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-resource "local_file" "values" {
-  content = templatefile("${path.module}/helm-app-template/${local.app_name}.yaml",
+module "application" {
+  source = "git::https://github.com/provectus/sak-incubator//meta-aws-application?ref=main"
+
+  chart_version = var.chart_version
+  repository    = var.chart_repository
+  name          = var.chart_name
+  chart         = var.chart_name
+  namespace     = var.namespace
+
+  iam_permissions = [
     {
-      chart_values               = local.chart_values
-      chart_repo                 = var.chart_repository
-      chart_version              = var.chart_version
-      module_name                = local.module_name
-      app_name                   = local.app_name
-      app_namespace              = var.namespace
-      argo_namespace             = var.argocd.namespace
-      chart_parameters           = var.chart_parameters
-      chart_parameters_as_string = var.chart_parameters_as_string
-  })
-  file_permission      = "0644"
-  directory_permission = "0755"
-  filename             = "${path.root}/${var.argocd.path}/${local.app_name}.yaml"
+      "Effect" : "Allow",
+      "Action" : "ssm:GetParameter",
+      "Resource" : "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.allowed_secrets_prefix}*"
+    },
+    {
+      "Effect" : "Allow",
+      "Action" : [
+        "secretsmanager:GetResourcePolicy",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:ListSecretVersionIds"
+      ],
+      "Resource" : [
+        "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.allowed_secrets_prefix}*"
+      ]
+    }
+  ]
+
+  irsa_annotation_field = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  cluster_name          = var.cluster_name
+  values = merge({
+    "securityContext.runAsNonRoot"     = true
+    "securityContext.fsGroup"          = 65534
+    "env.AWS_REGION"                   = data.aws_region.current.name
+    "env.AWS_DEFAULT_REGION"           = data.aws_region.current.name
+    "env.POLLER_INTERVAL_MILLISECONDS" = var.poller_interval
+  }, var.chart_values)
+
+  argocd = var.argocd
 }
--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@ variable "namespace" {
   description = "A name of the existing namespace"
 }
 
-variable "aws_region" {
-  type        = string
-  description = "A name of the AWS region (us-central-1, us-west-2 and etc.)"
-  default     = ""
-}
-
 variable "chart_version" {
   type        = string
   description = "A Helm Chart version"
@@ -27,43 +21,22 @@ variable "cluster_name" {
   description = "Name of the EKS cluster"
 }
 
-variable "cluster_oidc_url" {
-  type        = string
-  description = "oidc issuer url of the eks cluster"
-}
-
 variable "chart_repository" {
   type        = string
   description = "A chart repository"
   default     = "https://external-secrets.github.io/kubernetes-external-secrets/"
 }
 
+variable "chart_name" {
+  type        = string
+  description = "A name of the chart"
+  default     = "kubernetes-external-secrets"
+}
+
 variable "chart_values" {
-  default     = ""
+  type        = map(string)
+  default     = {}
   description = "Chart values"
-}
-
-variable "aws_assume_role_arn" {
-  default     = ""
-  description = "A role to assume. If left blank a new role will be created"
-}
-
-variable "secret_manager_full_access" {
-  default     = false
-  description = <<EOT
-  By setting this to true, the assumable role that is created for the Service Account attached to the External-Secrets application will have full access to all AWS Sercret Manager keys prefixed the name of the cluster. 
-  We recommend setting this to false and instead creating roles with fine-granular access policies for each ExternalSecret you define, and allow the role created here to assume those roles.
-  EOT
-}
-
-variable "chart_parameters" {
-  default     = []
-  description = "A list of parameters that will override defaults"
-}
-
-variable "chart_parameters_as_string" {
-  default     = []
-  description = "A list of parameters that will override defaults"
 }
 
 variable "tags" {
@@ -75,8 +48,9 @@ variable "tags" {
 variable "allowed_secrets_prefix" {
   type        = string
   description = "Prefix of for secrets we should be able to access from the external-secrets app?"
-  default     = "/eks/"
+  default     = ""
 }
+
 variable "argocd" {
   type        = map(string)
   description = "A set of values for enabling deployment through ArgoCD"
